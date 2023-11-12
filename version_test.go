5842a4535e785f5210adc4d2184665d6c4d04ff5
MondeSinxi <monde.sinxi@gmail.com>
2023-11-12 16:48:08 +0200

    TST: adds testing

--- version_test.go
+++ version_test.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-  "testing"
+	"regexp"
+	"testing"
 )
 
 func TestBumpMajor(t *testing.T) {
-  version := "0.1.0"
-  want := "1.0.0" 
+	want := regexp.MustCompile("1.0.0")
 
-  version.
+	version, err := BumpVersion("major", "VERSION")
+	if !want.MatchString(version) || err != nil {
+		t.Fatalf(`Expected 1.0.0, got %s instead`, version)
+
+	}
+}
+
+func TestBumpMinor(t *testing.T) {
+	want := regexp.MustCompile("0.2.0")
+
+	version, err := BumpVersion("minor", "VERSION")
+	if !want.MatchString(version) || err != nil {
+		t.Fatalf(`Expected 0.2.0, got %s instead`, version)
+
+	}
+}
+
+func TestBumpPatch(t *testing.T) {
+	want := regexp.MustCompile("0.1.1")
+
+	version, err := BumpVersion("patch", "VERSION")
+	if !want.MatchString(version) || err != nil {
+		t.Fatalf(`Expected 0.1.1, got %s instead`, version)
+
+	}
 }
